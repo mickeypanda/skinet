c0d4bb2d34f54a822c2b60ccd7b97d89635790ea
mickeypanda <pk.panda7381@gmail.com>
2020-10-03 19:29:51 +0530

    added ProductUrlResolver class to get the proper pictureUrl of the product and used this class in the AutoMapper profile class for a project.

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -16,7 +16,8 @@ namespace API.Helpers
             //This method is for creating the map between objects, from source to destinationas
             CreateMap<Product, ProductToReturnDto>()
                 .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name)) //ForMember is used for mapping the propeties according to the requirement.
-                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name));
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>()); //Used ProductUrlResolver to map and resolve the picture URLS as per complete urls.s
         }
     }
 }
---
+++ API/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using AutoMapper;
+using Core.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Helpers
+{
+    public class ProductUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
+    {
+        //This should be of type Microsoft.Extensions.Configuration
+        private readonly IConfiguration _config;
+
+        public ProductUrlResolver(IConfiguration config)
+        {
+            _config = config;
+        }
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                //Getting the ApiUrl string from the config setting.
+                return _config["ApiUrl"] + source.PictureUrl;
+            }
+            return null;
+        }
+    }
+}
