de8981c27200f4dbb6f9bd901726b840f1dbb222
mickeypanda <pk.panda7381@gmail.com>
2020-10-07 00:18:26 +0530

    added code for handling the Apiresponse validation errors

---
+++ API/Errors/ApiValidationErrorResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Errors
+{
+    /// <summary>
+    /// This class will be used for handling the validation errors(400) for the service requests.
+    /// The IEnumerable property will be set from Startup.cs class.
+    /// </summary>
+    public class ApiValidationErrorResponse : ApiResponse
+    {
+        public ApiValidationErrorResponse() : base(400)
+        {
+        }
+
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Errors;
 using API.Helpers;
 using API.Middlewares;
 using AutoMapper;
@@ -40,6 +41,25 @@ namespace API
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
+
+            //Code for setting up the I Enumerable properties for ApiValidation errors.
+            services.Configure<ApiBehaviorOptions>(option =>
+            {
+                option.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    var errors = actionContext.ModelState
+                    .Where(e => e.Value.Errors.Count > 0)
+                    .SelectMany(x => x.Value.Errors)
+                    .Select(x => x.ErrorMessage).ToArray();
+
+                    var errorResponse = new ApiValidationErrorResponse
+                    {
+                        Errors = errors
+                    };
+
+                    return new BadRequestObjectResult(errorResponse);
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
